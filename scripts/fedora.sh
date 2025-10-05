41f2526a00e31b7a1dc4f5a9fbe39445d35123a1
mkharis <081332210xxx@gmail.com>
2025-10-05 12:51:08 +0700

    fix copr

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -4,6 +4,8 @@
 ./distrobox-shims.sh
 
 # Upgrade the container and install packages
-dnf copr enable alternateved/eza atim/lazygit atim/starship -y
+dnf copr enable alternateved/eza -y
+dnf copr enable atim/lazygit -y
+dnf copr enable atim/starship -y
 dnf upgrade -y
 grep -v '^#' ./fedora.packages | xargs dnf install -y
