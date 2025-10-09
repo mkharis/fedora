266b6a85feeb12cb59cdf4576b3f73321b4fefe9
Mukhammad Kharis <mkharis@users.noreply.github.com>
2025-10-09 14:58:11 +0700

    update distrobox-shims.sh

--- scripts/distrobox-shims.sh
+++ scripts/distrobox-shims.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 [ ! -e /usr/bin/sh ] && ln -fs /bin/sh /usr/bin/sh
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/distrobox
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/docker
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
