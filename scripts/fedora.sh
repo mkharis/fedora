be2e3b0d5d49facde9fc48be9dc63dc47a3b4f80
Mukhammad Kharis <081332210xxx@gmail.com>
2026-01-30 06:59:58 +0700

    add lazydocker package

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -5,6 +5,7 @@
 
 # Upgrade the container and install packages
 dnf copr enable alternateved/eza -y
+dnf copr enable atim/lazydocker -y
 dnf copr enable atim/lazygit -y
 dnf copr enable atim/starship -y
 dnf upgrade -y
