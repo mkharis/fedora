22b6304e06d47bc6cc0b14eb0f01cfb2f4c7970f
Mukhammad Kharis <mkharis@users.noreply.github.com>
2026-05-15 14:34:02 +0700

    Remove enabling of 'alternateved/eza' COPR repository
    Removed enabling of the 'alternateved/eza' COPR repository.

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -4,7 +4,6 @@
 ./distrobox-shims.sh
 
 # Upgrade the container and install packages
-dnf copr enable alternateved/eza -y
 dnf copr enable atim/lazygit -y
 dnf copr enable atim/starship -y
 dnf upgrade -y
